e7eb62c3032f95fcb3a9409d8df8499ee773eca3
Boyu Jiang <jbyjiangboyu@126.com>
2018-12-08 08:43:46 -0800

    chore(app): change name 'pic_link' to 'img_link'

--- vgg16.py
+++ vgg16.py
@@ -2,10 +2,11 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-def get_image_data(image_link):
-    response = requests.get(iamge_link)
+def get_image_data(img_link):
+    response = requests.get(img_link)
     img = Image.open(BytesIO(response.content))
     data = list(img.getdata())
     return data
 
-get_image_data("https://i.ebayimg.com/thumbs/images/m/mBiLStlJ3REaZ9KLFG6MSrQ/s-l225.jpg")
+
+print (get_image_data("https://i.ebayimg.com/thumbs/images/m/mBiLStlJ3REaZ9KLFG6MSrQ/s-l225.jpg"))
