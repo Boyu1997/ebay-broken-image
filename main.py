89682fa4a5f938845d05c42c21581bf64a59fbb8
Boyu Jiang <jbyjiangboyu@126.com>
2018-12-08 08:30:46 -0800

    feat(main): add request args when calling beautifulsoup

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from beautifulsoup import main as beautifulsoup
-
+from flask import request
 import flask
 
 app = flask.Flask(__name__)
 
-with app.app_context():
-    data = beautifulsoup(None)
+with app.test_request_context(method='POST'):
+    # known keywords: ['hat', 'shirt']
+    # known buggy keywords: ['phone', 'toy', 'cup', 'camera', 'calculator']
+    request.args = {'keyword': 'shirt'}
+    data = beautifulsoup(request)
     print (data.get_json())
