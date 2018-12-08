6897dd96a9e67f15e6fa33329b84d86a20432aba
Boyu Jiang <jbyjiangboyu@126.com>
2018-12-08 09:06:27 -0800

    feat(vgg16): image to feature using vgg16

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from beautifulsoup import main as beautifulsoup
+from vgg16 import main as vgg16
 from flask import request
 import flask
 
@@ -8,5 +9,10 @@ with app.test_request_context(method='POST'):
     # known keywords: ['hat', 'shirt']
     # known buggy keywords: ['phone', 'toy', 'cup', 'camera', 'calculator']
     request.args = {'keyword': 'shirt'}
-    data = beautifulsoup(request)
-    print (data.get_json())
+    response = beautifulsoup(request)
+    data = response.get_json()
+
+with app.test_request_context(method='POST'):
+    request.args = {'data': data}
+    response = vgg16(request)
+    data = response.get_json()
