40e750cdc1a525fbeb0842bed174a9cd7bef3da4
Boyu Jiang <jbyjiangboyu@126.com>
2018-12-14 09:38:16 -0800

    feat(vgg16): setup vgg16 for gcp and ml engine

--- vgg16/gcp_request.py
+++ vgg16/gcp_request.py
@@ -1,7 +1,12 @@
 from oauth2client.client import GoogleCredentials
 from googleapiclient import discovery
 
-def ml_predict(instances, projectID, modelID, instancesID, versionID=None):
+# Clould ML identifier
+projectID = 'vertical-sunset-186521'
+modelID = 'ebay_vgg16_mlengine'
+versionID = 'v_2018_12_13_07_14'
+
+def ml_predict(instances):
 
     # build a representation of the Google Cloud ML API
     ml = discovery.build('ml', 'v1')
@@ -15,7 +20,20 @@ def ml_predict(instances, projectID, modelID, instancesID, versionID=None):
         body={'instances': [instances]}
     ).execute()
 
+    # handle exception
     if 'error' in response:
         raise RuntimeError(response['error'])
 
+    # return data
     return response['predictions']
+
+
+def get_request(request, key, default=None):
+    request_json = request.get_json()
+    if request.args and key in request.args:
+        data = request.args.get(key)
+    elif request_json and key in request_json:
+        data = request_json[key]
+    else:
+        data = default
+    return data
--- vgg16/main.py
+++ vgg16/main.py
@@ -1,81 +1,18 @@
-from PIL import Image
-import requests
-from io import BytesIO
-import numpy as np
-from keras.applications.vgg16 import VGG16
 import flask
 from flask import request
-
 import json
-import pandas as pd
 
-from gcp_mlengine_request import ml_predict
+from gcp_request import ml_predict, get_request
 
-def get_image_data(img_link):
-    response = requests.get(img_link)
-    img = Image.open(BytesIO(response.content))
-    img = img.convert('RGB')
-    img = img.resize((224,224))
-    data = np.array(list(img.getdata()))
-    data = data.reshape(224, 224, 3)
-    return data
+def ebay_vgg16(request):
 
-
-
-def ebay_vgg16(request, local=False, local_data=False):
-
-    # obtain image data
-    if local_data:
-        img_set = np.load("test.npy")
-    else:
-        if request is None:
-            return None
-        else:
-            request_json = request.get_json()
-            if request.args and 'data' in request.args:
-                data = request.args.get('data')
-            elif request_json and 'data' in request_json:
-                data = request_json['data']
-            else:
-                return None
-
-        img_set = []
-        for d in data:
-            img_data = get_image_data(d['img_link'])
-            img_set.append(img_data)
-
-        print ("Total {:d} image data".format(len(img_set)))
-        img_set = np.array(img_set)
-        np.save("test.npy", img_set)
+    # if keyward passed by the request object, update keyward
+    if request is not None:
+        # get keyword
+        img_data = get_request(request, 'img_data')
 
     # conver image into feature using vgg16
-    if local:
-        model = VGG16(weights='imagenet', include_top=False, input_shape=(224,224,3))
-        feature = model.predict(img_set)
-        feature = feature.tolist()
+    instances = img_data
+    prediction = ml_predict(instances)
 
-    else:
-        # define Clould ML identifier
-        projectID = 'vertical-sunset-186521'
-        modelID = 'ebay_vgg16_mlengine'
-        versionID = 'v_2018_12_13_07_14'
-
-        feature = []
-        print ("{:d} iamge to process".format(img_set.shape[0]))
-        for i in range(img_set.shape[0]):
-            instances = (img_set[i]).tolist()
-            this_prediction = ml_predict(instances, projectID, modelID, versionID)
-            print ("image {:d} completed".format(i))
-            feature.append(this_prediction)
-
-        return
-
-    return flask.jsonify(feature)
-
-
-# code for local testing
-if __name__=='__main__':
-    app = flask.Flask(__name__)
-    with app.test_request_context(method='POST'):
-        request.args = {'data': None}
-        ebay_vgg16(request, local_data=True)
+    return flask.jsonify(prediction)
